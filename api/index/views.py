9a666c6876b731b9fcdd1ce3fb8131ce25fb5454
prashanth <prashanthr6789@gmail.com>
2022-11-13 19:33:02 -0800

    added welcome page and minor changes

--- api/index/views.py
+++ api/index/views.py
@@ -1,12 +1,10 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+from django.http import HttpResponse
 from rest_framework.decorators import api_view
 
 # Create your views here.
 @api_view(['GET'])
 def welcome(request):
     if request.method == 'GET':
-        return JsonResponse({
-            'response': 'welcome'
-        })
+        return render(request, "index/welcome.html")
         
