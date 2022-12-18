f2ea0dbecbbedb6012495342b4c2833c05fab4ac
prashanthkumarreddy.busupal523 <prashanthkumarreddy.busupal523@csuci.edu>
2022-12-18 12:20:16 -0800

    added home api and changed homepage

--- api/index/urls.py
+++ api/index/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.welcome, name='welcome'),
-    path('ref', views.tst, name='tst'),
+    path('ref/', views.tst, name='tst'),
+    path('home/', views.tst, name='tst'),
 ]
